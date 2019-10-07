40e830f8cb297c5511d6808b79e7154ad8efd39e
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2019-10-07 12:40:30 +0200

    version 1.0

--- tareaAgenda.php
+++ tareaAgenda.php
@@ -1,3 +1,6 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,33 +9,53 @@
 <body>
 	<h1>Agenda Web</h1>
 <form method="POST" action="#">
-	<label>Nombre</label><input type="text" name="nombre" id="nombre"><br>
-	<label>Correo electronico</label><input type="email" name="correo" id="correo">
+	<label>Nombre</label><input type="text" name="nombre" id="nombre"
+	<?php
+		if (isset($_POST["nombre"]) && trim($_POST["nombre"]) != ""){
+			echo("value = '" . $_POST["nombre"]) . "'";
+		}
+	?>><br>
+	<label>Correo electronico</label><input type="email" name="correo" id="correo"
+	<?php
+		if (isset($_POST["correo"]) && trim($_POST["correo"]) != ""){
+			echo("value = '" . $_POST["correo"]) . "'";
+		}
+	?>>
 	<input type="submit" name="enviar" value="Enviar">
 </form><br><br>
 <ul>
 	<?php
-		session_start();
+		//session_start();
 		if (isset($_SESSION["usuarios"])){
-			echo("una vez creada la sesion");
 			$usuarios = $_SESSION["usuarios"];
 			if ($_POST["enviar"]){
-				if (isset($_POST["nombre"]) && isset($_POST["correo"]) && trim($_POST["nombre"]) != '' && trim($_POST["correo"]) != ""){
-					$usuarios[$_POST["nombre"]] = $_POST["correo"];
-					echo(count($usuarios));
-				}else if (!isset($_POST["correo"]) || trim($_POST["correo"]) == ""){
-					unset($usuarios[$_POST["nombre"]]);
-					$usuarios = array_values($usuarios);
-					$_SESSION["usuarios"] = $usuarios;
+				if (trim($_POST["nombre"]) != "" && trim($_POST["correo"]) != ""){
+					$usuarioRepetido = false;
+					for ($i = 0; $i<count($usuarios); $i++){
+						if ($usuarios[$i]["nombre"] == $_POST["nombre"]){
+							$usuarioRepetido = true;
+							$usuarios[$i]["correo"] = $_POST["correo"];
+						}
+					}
+					if (!$usuarioRepetido){
+						$persona = array("nombre" => $_POST["nombre"], "correo" => $_POST["correo"]);
+						array_push($usuarios, $persona);
+					}
+				}else if (trim($_POST["nombre"]) != ""){
+					for ($i = 0; $i<count($usuarios); $i++){
+						if ($usuarios[$i]["nombre"] == $_POST["nombre"]){
+							unset($usuarios[$i]);
+						}
+					}
 				}else{
-					echo("Inserta el nombre");
+					echo("Inserta como mínimo el nombre");
 				}
+				$_SESSION["usuarios"] = $usuarios;
 			}
-			foreach ($usuarios as $key => $value) {
-				echo($key . " " . $value . "<br>");
+			foreach ($usuarios as $persona) {
+				echo($persona["nombre"] . " " . $persona["correo"] . "<br>");
 			}
 		}else{
-			echo("sin crear sesion");
 			$usuarios = array();
 			$_SESSION["usuarios"] = $usuarios;
 		}
